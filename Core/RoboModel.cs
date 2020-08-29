df5e63ce6c3fa1517099888290f6a9b88e6efaa8
Andris Tapins <andris.tapins@outlook.com>
2020-08-30 02:59:38 +0300

    Initial

---
+++ Core/RoboModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class RoboContext : DbContext
+    {
+        public DbSet<Solution> Solutions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=/db/robodb.db");
+    }
+
+    class Solution
+    {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
